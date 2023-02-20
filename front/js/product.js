12d0e8918c96eb9f0751cc38ed569f974579b843
Sebastien Hartmann <coach.habitudes.apprentissages@gmail.com>
2023-02-20 15:40:46 +0100

    création de la const image dans le js et de ses attributs + modification balise HTML avec id

--- front/js/product.js
+++ front/js/product.js
@@ -6,7 +6,7 @@ fetch("http://localhost:3000/api/products")
     products.forEach(product => {
       const article = document.createElement("article");
       const link = document.createElement("a");
-      link.setAttribute("href", `./product.html?id=${product.id}`);
+      link.setAttribute("href", `./product.html?id=${product._id}`);
       const image = document.createElement("img");
       image.setAttribute("src", product.imageUrl);
       image.setAttribute("alt", product.name);
@@ -23,4 +23,34 @@ fetch("http://localhost:3000/api/products")
       article.appendChild(productDescription);
       itemsSection.appendChild(link);
     });
+  });
+
+
+const params = new URLSearchParams(window.location.search);
+const productId = params.get('id');
+
+fetch(`http://localhost:3000/api/products/${productId}`)
+  .then(response => response.json())
+  .then(product => {
+    // Mettre à jour les éléments HTML avec les détails du produit
+    document.getElementById('title').textContent = product.name;
+    document.getElementById('price').textContent = product.price;
+    document.getElementById('description').textContent = product.description;
+    
+    // Mettre à jours l'élément HTML correspondant à l'image et son alt
+    const image = document.getElementById('image');
+    image.setAttribute('src', product.imageUrl);
+    image.setAttribute('alt', product.name);
+
+    // Ajouter les options de couleur à la liste déroulante
+    const colorsSelect = document.getElementById('colors');
+    product.colors.forEach(color => {
+      const option = document.createElement('option');
+      option.value = color;
+      option.textContent = color;
+      colorsSelect.appendChild(option);
+    });
+  })
+  .catch(error => {
+    console.error('Une erreur s\'est produite :', error);
   });
